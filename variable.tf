b58292ad728a7ed1a2d28b7743b5ecd1ea60b4ac
root <root@ip-172-31-64-155.ec2.internal>
2020-09-10 19:42:28 +0000

    commit

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "public_key_file" {
+  type        = string
+  default     = "/home/ubuntu/pubkey"
+}
+
+variable "master_instance_type"{
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "worker_instance_type"{
+  type        = string
+  default     = "t2.micro"
+}
